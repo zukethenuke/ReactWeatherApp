e265978c6d977940edede75afd2f096f3aa6e2fa
zukethenuke <zukethenuke@hotmail.com>
2017-04-19 10:08:51 -0400

    Add style to nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,13 +3,43 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = (props) => {
   return (
-      <div>
-        <h2>Nav Componet</h2>
-        <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to='about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to='examples' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-      </div>
+        <div className='top-bar'>
+          <div className='top-bar-left'>
+            <ul className='menu'>
+              <li>
+                <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              </li>
+              <li>
+                <Link to='about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
+              </li>
+              <li>
+                <Link to='examples' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+              </li>
+            </ul>
+          </div>
+          <div className='top-bar-right'>
+            
+          </div>
+        </div>
     );
 }
 
 module.exports = Nav;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <div>
+//   <h2>Nav Componet</h2>
+//   
+// </div>
