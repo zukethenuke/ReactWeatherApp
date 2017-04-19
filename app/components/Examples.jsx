73052c8f609aa87f396489a1202a4ef061c1e38a
zukethenuke <zukethenuke@hotmail.com>
2017-04-19 12:51:33 -0400

    Add locations to examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to the Examples page!</p>
+        <h1 className='text-canter'>Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Omaha'>Omaha, NE</Link>
+          </li>
+          <li>
+            <Link to='/?location=Arlington'>Arlington, VA</Link>
+          </li>
+        </ol>
       </div>
     )
 }
