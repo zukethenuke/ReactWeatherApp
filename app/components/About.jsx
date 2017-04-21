c32f381c57f0b96d739895ccd9c3d163f626d524
zukethenuke <zukethenuke@hotmail.com>
2017-04-21 19:38:32 -0400

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,18 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
 return (
      <div>
-        <h3>About</h3>
-        <p>Weolcome to the About page</p>
+        <h1 className='text-center'>About</h1>
+        <p>Tools used to build this app:</p>
+        <ol>
+          <li><Link to='https://facebook.github.io/react/'>React</Link></li>
+          <li><Link to='https://nodejs.org/en/'>Node.js</Link></li>
+          <li><Link to='https://openweathermap.org/'>Open Weather Map</Link></li>
+          <li><Link to='http://foundation.zurb.com/'>Foundation</Link></li>
+          <li><Link to='https://github.com/zukethenuke/ReactWeatherApp'>Github</Link></li>
+        </ol>
      </div>
     )
 }
